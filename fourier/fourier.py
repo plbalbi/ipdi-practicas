fa6ba3064d8b8f4bf97ea76fe118e95fe79d8a76
pab <pablo.l.balbi@gmail.com>
2017-10-11 00:03:15 -0300

    separando fouerir y main

--- fourier/fourier.py
+++ fourier/fourier.py
@@ -1,8 +1,4 @@
 import numpy as np
-from scipy import misc
-from matplotlib import pyplot as plt
-import sys
-
 # convolucion
 def convolve(I, m):
 # def convolve(I, m, z_padding = True):
@@ -80,23 +76,3 @@ def convolve(I, m):
 def fourier_transf():
     return
     # transformada
-
-
-
-test_img = misc.imread(sys.argv[1])
-plt.subplot(1,2,1)
-plt.imshow(test_img, cmap='gray')
-plt.subplot(1,2,2)
-
-M_KERN = np.array([\
-        [-1, 0, 1],\
-        [-2, 0, 2],\
-        [-1, 0, 1]\
-        ], dtype = np.int)
-
-M_KERN_AVG = (1/9)*np.ones([3,3])
-print(M_KERN)
-# import pdb; pdb.set_trace()
-conv_img = convolve(test_img, M_KERN)
-plt.imshow(conv_img, cmap='gray')
-plt.show()
