32f6663ab49110a3b35b0477c418c8297dca24aa
npazosmendez <npazosmendez@gmail.com>
2017-10-18 11:15:59 -0300

    ejs

--- p3/ejs.py
+++ p3/ejs.py
@@ -9,6 +9,7 @@ from scipy.fftpack import ifft2, fft2
 from scipy import misc
 from scipy import fftpack
 from scipy import signal
+from scipy import ndimage
 import sys
 import ecualizacion as equ
 
@@ -149,13 +150,15 @@ def ej3():
         # plt.show()
 
 def ej4():
-    if len(sys.argv) != 3:
-        print("faltan params")
-        print("llamar como >"+ '\033[1m' +" %s imagen1 imagen2" % (sys.argv[0])+ '\033[0m')
-        sys.exit(1)
+    # if len(sys.argv) != 3:
+    #     print("faltan params")
+    #     print("llamar como >"+ '\033[1m' +" %s imagen1 imagen2" % (sys.argv[0])+ '\033[0m')
+    #     sys.exit(1)
     # fft2
-    im1 = misc.imread(sys.argv[1])
-    im2 = misc.imread(sys.argv[2])
+    # im1 = misc.imread(sys.argv[1])
+    # im2 = misc.imread(sys.argv[2])
+    im1 = misc.imread('lena.png')
+    im2 = misc.imread('ladrillos.png')
     im1_FT = fft2(im1)
     im2_FT = fft2(im2)
     im1_norm, im1_angle = uncompress_cmpx(im1_FT)
@@ -215,7 +218,7 @@ def ej4():
     # print(np.uint8(np.real(temp)))
 
 def ej5():
-    lena_route = "../ImagenesHistograma/lena.png"
+    lena_route = "lena.png"
     lena_img = misc.imread(lena_route)
     lena_FFT = fft2(lena_img)
     # lineas horizontales
@@ -230,24 +233,28 @@ def ej5():
 def ej6():
     f = [random.randint(0,5) for i in range(10)]
     g = [random.randint(0,5) for i in range(10)]
-    fg = signal.convolve(g,f)
+    fg = ndimage.convolve(g,f,mode='wrap')
+    F1 = fftpack.fft(fg)
 
-    F1 = fourier.DFT(fg)
+    F2 = np.multiply(fftpack.fft(f),fftpack.fft(g))
 
-    F2 = np.multiply(fourier.DFT(f),fourier.DFT(g))
-
-    plt.plot(F1,'go')
-    plt.plot(F2,'ro')
+    plt.subplot(2,2,1)
+    plt.plot(F1,'go',label='F(f*g)',alpha=0.5)
+    plt.legend()
+    plt.subplot(2,2,2)
+    plt.plot(F2,'ro',label='F(f).F(g)',alpha=0.5)
+    plt.legend()
+    plt.subplot(2,2,3)
+    plt.plot(f,'go',label='F(f*g)',alpha=0.5)
+    plt.subplot(2,2,4)
+    plt.plot(g,'go',label='F(f*g)',alpha=0.5)
     plt.show()
 
 
 def test_norms():
     # con 2 de gamma se ve bien
-    img = misc.imread(sys.argv[1])
-    if len(sys.argv) > 2:
-        gamma = float(sys.argv[2])
-    else:
-        gamma = 1
+    img = misc.imread('lena.png')
+    gamma = 2
     img_FFT = fft2(img)
     plt.subplot(1,3,1)
     plt.imshow(img, cmap='gray')
